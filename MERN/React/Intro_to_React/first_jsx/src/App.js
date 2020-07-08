94018eb4316adf4ff3f7c27879504ded2aa8591a
Fabian <gonzalezfabian91@gmail.com>
2020-07-08 16:04:18 -0700

    first_jsx completed

---
+++ MERN/React/Intro_to_React/first_jsx/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+        <h1>Hello Dojo!</h1>
+        <h3>Things i need to do:</h3>
+        <ul>
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a Marathon</li>
+            <li>Feed the dogs</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
