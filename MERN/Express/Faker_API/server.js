1a2094a135d3b2577b29eefbb13350338b81132a
Fabian <gonzalezfabian91@gmail.com>
2020-07-16 19:44:41 -0700

    faker api completed

---
+++ MERN/Express/Faker_API/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const faker = require('faker');
+const app = express();
+
+class User {
+    constructor(){
+        this.fname = faker.name.firstName();
+        this.lname = faker.name.lastName();
+        this.job = faker.name.title();
+    }
+}
+
+class Company {
+    constructor(){
+        this.cname = faker.company.companyName();
+        this.phrase = faker.company.catchPhrase();
+        this.bs = faker.company.bs();
+    }
+}
+
+app.get("/api/user/new", (req, res) => {
+    res.json(new User());
+});
+
+app.get("/api/companies/new", (req, res) => {
+    res.json(new Company());
+});
+
+//still need to get this one to show both User and Company
+app.get("/api/user/company", (req, res) => {
+    res.json(new User());
+    // res.json(new Company());
+});
+
+app.listen(8000, () => {
+    console.log("listening");
+})
